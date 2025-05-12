55b83f3d55b1e41d448c17ea2f81a9342a2c3872
Emils <emils@gmail.com>
2025-05-12 16:36:43 +0300

    dzēst metode pārlikta uz show lapu

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -7,11 +7,7 @@
             <div>
                 <a href="/books/{{ $book->id }}">Show</a>
                 <a href="/books/{{ $book->id }}/edit">Edit</a>
-                <form action="/books/{{$book->id}}" method="post">
-                    @csrf
-                    @method('delete')
-                    <input type="submit" value="dzēst">
-                </form>
+                
             </div>
         </li>
     @endforeach
--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -2,3 +2,9 @@
 <h3>{{ $book->author }}</h3>
 <p>{{ $book->released_at }}</p>
 <a href="/books">All books</a>
+
+<form action="/books/{{$book->id}}" method="post">
+                    @csrf
+                    @method('delete')
+                    <input type="submit" value="dzēst">
+                </form>
